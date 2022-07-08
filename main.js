9c925a33d6a9db60380bb5a306677b3695a9083e
Bhavyaasharma <81627599+Bhavyaasharma@users.noreply.github.com>
2022-07-08 15:51:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,18 +20,18 @@ function draw()
 {
     image(video,0,0,480,380);
      
-    if (status !=" ")
+    if (status != "")
     {
         objectDetector.detect(video, gotResult);
-        for(i= 0; i < object.length; i++){
+        for(i= 0; i < objects.length; i++){
             document.getElementById("status").innerHTML = "status : object dectected"
-            document.getElementById("numberOfObject").innerHTML = "number of object detected are : " +object.length;
+            document.getElementById("numberOfObject").innerHTML = "number of object detected are : " +objects.length;
 
             fill("#FF0000");
             percent = floor(objects [i].confidence*100);
-            text(object [i].label +" " +percent + "%", objects [i].x + 15 , objects [i].y + 15);
+            text(objects [i].label +" " +percent + "%", objects [i].x + 15 , objects [i].y + 15);
             noFill();
-            stroke("FF0000");
+            stroke("#FF0000");
             rect(objects [i].x,objects [i].y,objects [i].width, objects [i].height);
         
         }
